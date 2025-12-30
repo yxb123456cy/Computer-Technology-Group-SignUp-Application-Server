0fb8b9144a9439e9dd6cba4e9e1a342760b2ab1c
yxb123456cy <2186256471@qq.com>
2025-12-31 00:44:01 +0800

    feat(init): init SpringBoot3+JDK21 Project

---
+++ src/main/java/com/lemon/computer/ComputerTechnologyGroupSignUpApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.lemon.computer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerTechnologyGroupSignUpApplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerTechnologyGroupSignUpApplicationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lemon/computer/ComputerTechnologyGroupSignUpApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemon.computer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerTechnologyGroupSignUpApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
